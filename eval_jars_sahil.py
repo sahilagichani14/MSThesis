626278cde9290901db5050955777965174e8795d
sahilagichani14 <sahilagichani@gmail.com>
2024-10-26 18:17:52 +0200

    reverse order & ls,tr twice

--- eval_jars_sahil.py
+++ eval_jars_sahil.py
@@ -10,11 +10,11 @@ import concurrent.futures
 # Get the current directory of eval_jars.py
 k_configuration = 2
 input_dir = join(getcwd(), 'IDELinearConstantAnalysisClientSoot/src/test/resources/latest')
-input_dir = join(getcwd(), 'IDELinearConstantAnalysisClientSootUp/src/test/resources/latest')
+# input_dir = join(getcwd(), 'IDELinearConstantAnalysisClientSootUp/src/test/resources/latest')
 output_directory = join(getcwd(), 'evalresults')
 
 executable = join(getcwd(), "eval_results/IDELinearConstantAnalysisClientSoot-1.0-SNAPSHOT-jar-with-dependencies.jar")
-executable = join(getcwd(), "eval_results/IDELinearConstantAnalysisClientSootUp-1.0-SNAPSHOT-jar-with-dependencies.jar")
+# executable = join(getcwd(), "eval_results/IDELinearConstantAnalysisClientSootUp-1.0-SNAPSHOT-jar-with-dependencies.jar")
 
 eval_csv_file = join(getcwd(), "evalresults", "max_heap_ide_default.csv")
 
@@ -34,7 +34,7 @@ allbodyinterceptors = ["jb.lp", "jb.ese", "jb.ne", "jb.dae", "jb.ule", "jb.cp", 
 # testng-7.7.1.jar default 100 RTA 1 jb.ls,jb.tr
 command = ["java", "-Xmx1024m", "-Xss1024m", "-jar", executable, "inputjar" , "solver", max_method, "cg_algo", "thread", "bodyinterceptors"]  # to be set programmatically
 command = ["java", "-XX:+UseG1GC", "-XX:+UseAdaptiveSizePolicy", "-Xmx1024m", "-Xss1024m", "-jar", executable, "inputjar" , "solver", max_method, "cg_algo", "thread", "bodyinterceptors"]  # to be set programmatically
-command = ["java", "-XX:+UseG1GC", "-XX:+UseAdaptiveSizePolicy", "-Xmx1024m", "-Xss1024m", "-jar", executable, "inputjar" , "solver", max_method, "cg_algo", "thread"]
+# command = ["java", "-XX:+UseG1GC", "-XX:+UseAdaptiveSizePolicy", "-Xmx1024m", "-Xss1024m", "-jar", executable, "inputjar" , "solver", max_method, "cg_algo", "thread"]
 
 # jar,solver,thread,totalRuntime,cgConstructionTime,prop,method,mem,CallGraphAlgo,callGraphEdges,callGraphReachableNodes,initialStmtCount,stmtCountAfterApplyingBI,BodyTransformers,BodyTransformersMetrics
 # testng-7.7.1,default,1,186237,15568,7992782,100,962,RTA,42422,6343,68170,56616,[JB_LS, JB_TR, JB_LP, JB_CP, JB_DAE, JB_ESE, JB_CBF, JB_UCE],{jb.cp=[270, -22], jb.tr=[1241, -124], jb.uce=[683, 41], jb.ls=[415, 54], jb.dae=[276, -162]}
